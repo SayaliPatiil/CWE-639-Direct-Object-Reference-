c95aea781781df0d74122bef02a04c5611cef870
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-01-31 10:29:51 +0530

    Create DirectObjectReference.java

---
+++ DirectObjectReference.java
@@ -0,0 +1,13 @@
+package javacodechecker;
+
+
+
+public class DirectObjectReference{
+@RequestMapping(value = "/records/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+@Timed
+// EMB-ISSUE: CodeIssueNames.DIRECT_OBJECT_REFERENCE
+public ResponseEntity < Record > get(@PathVariable Long id) {
+return log.debug("REST request to get record" + id);
+}
+
+}
